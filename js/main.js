02935323d47a7a26d0a41dd81506e808408f481a
David <100007467.joan23@fje.edu>
2022-05-01 17:41:41 +0200

    1 commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-function Bin2Dec() {
+function texto() {
+    // Esto es para que los id conecten con el JavaScript
+    var error1 = document.getElementById('error1')
+    var error2 = document.getElementById('error2')
     var binario = document.getElementById('binario').value
-    var resultado = parseInt(binario, 2);
-    var parrafo = document.getElementById('decimal')
-    parrafo.innerHTML = resultado
-    if (parrafo.innerHTML == "NaN") {
-        parrafo.innerHTML = "Sigo esperando..."
-    }
-    var ultimodigito = substr(-1)
-    if (ultimodigito == 0) {
-        //binario
+    var resultado = document.getElementById('resultado')
+        // Esto es para que se transforme el numero a binario
+    resultado.innerHTML = parseInt(binario, 2);
+    // Esto es para que coja el ultimo valor
+    var ultimodigito = binario.charAt(binario.length - 1)
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        error2.innerHTML = 'Aqui esta tu conversion'
+        error1.innerHTML = ''
     } else {
-        parrafo.innerHTML = "Esto no es un numero binario BOBO"
+        resultado.innerHTML = 'Esperando a que pongas tu binario...'
     }
-
-
-
+    if (binario == '') {
+        procesando.innerHTML = ''
+    }
+    // Configuración de errores
+    if (ultimodigito >> 1 || ultimodigito >> 9999) {
+        error1.innerHTML = 'Has intruducio un numero no binario, introduce una serie de 1 y 0'
+        error2.innerHTML = ''
+    }
+    if (ultimodigito == '') {
+        error1.innerHTML = ''
+        error2.innerHTML = ''
+    }
+    // Para que en vez de NaN ponga Sigo esperando... (nose pork no va xd)
+    if (resultado.innerHTML == "NaN") {
+        resultado.innerHTML = "Sigo esperando..."
+    }
 }
